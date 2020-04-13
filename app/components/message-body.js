fc488422fb3c9884125be7009b941bf16f6ea4df
Sathish kumar <sathislxg@gmail.com>
2020-04-13 14:37:37 -0700

    Replace imgs with static assets and add messabody animation

--- app/components/message-body.js
+++ app/components/message-body.js
@@ -14,6 +14,9 @@ export default class extends Component {
     }
 
     willDestroy() {
+        const style = document.getElementById(`style-id-${this.args.message.id}`);
+
+        style.parentNode.removeChild(style);
         window.removeEventListener('click', this._handleClickOutside);
     }
 
@@ -23,9 +26,106 @@ export default class extends Component {
         }
     }
 
+    _listenForEndEvent(el, endEventType, cb) {
+        const onEnd = () => {
+            cb();
+            el.removeEventListener(endEventType, onEnd);
+        };
+
+        el.addEventListener(endEventType, onEnd);
+    }
+
+
+    _transitionToOpen(message, height) {
+        const grower = message.querySelector(".grower");
+        const pusher = message.querySelector(".pusher");
+
+        if (message.classList.contains("mail--open")) {
+            return;
+        }
+
+        message.classList.add("mail--opening");
+        pusher.classList.add("opening");
+        pusher.style.height = `${height}px`;
+
+        this._listenForEndEvent(grower, "animationend", () => {
+            message.classList.remove("mail--opening");
+            message.classList.add("mail--open");
+            pusher.classList.remove("opening");
+            pusher.style.height = 0;
+        });
+    }
+
+    _transitionToClosed(message) {
+        const pusher = message.querySelector(".pusher");
+
+        message.classList.add("mail--closing");
+        message.classList.remove("mail--open");
+        pusher.classList.add("closing");
+
+        this._listenForEndEvent(pusher, "animationend", () => {
+            message.classList.remove("mail--closing");
+            pusher.classList.remove("closing");
+        });
+    }
+
+
     @action
     _setRef(ref) {
         this.messageBody = ref;
+        const details = ref.querySelector(".mail-content-details");
+        const scale = (details.clientHeight + 47) / 47;
+        const sheet = (() => {
+            // Create the <style> tag
+            const style = document.createElement("style");
+            style.setAttribute("id", `style-id-${this.args.message.id}`);
+
+            // style.innerHTML = `
+            //     @keyframes scale-out {
+            //         0% {
+            //             transform: scale(1, 1);
+            //         }
+            //         80%,
+            //         100% {
+            //             transform: scale(1, ${scale});
+            //         }
+            //     }
+            // `;
+
+            // WebKit hack :(
+            style.appendChild(document.createTextNode(""));
+            document.head.appendChild(style);
+
+            return style.sheet;
+        })();
+
+
+
+        sheet.insertRule(`
+            @keyframes scale-out {
+                0% {
+                    transform: scale(1, 1);
+                }
+                80%,
+                100% {
+                    transform: scale(1, ${scale});
+                }
+            }
+        `, 0);
+
+        sheet.insertRule(`
+            @keyframes scale-in {
+                0% {
+                    transform: scale(1, ${scale});
+                }
+                80%,
+                100% {
+                    transform: scale(1, 1);
+                }
+            }
+        `, 1);
+
+        setTimeout(() => this._transitionToOpen(this.messageBody, details.clientHeight), 0);
     }
 
     @action
